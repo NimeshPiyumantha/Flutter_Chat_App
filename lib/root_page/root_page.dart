64b23097d9868a5973bba37ec4ae45146f92b2c4
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-18 22:58:12 +0530

    refactored project

---
+++ lib/root_page/root_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/screen/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePageView(),
+    );
+  }
+}
