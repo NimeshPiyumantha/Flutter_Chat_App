43a9fd25e513743d51dcdcb2e499d7db5739617d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-19 14:30:53 +0530

    fixed

--- lib/customUI/custom_chat_card.dart
+++ lib/customUI/custom_chat_card.dart
@@ -7,6 +7,7 @@ class CustomChatCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return const ListTile(
       leading: CircleAvatar(
+        backgroundImage: AssetImage('assets/images/boyOne.png'),
         radius: 30,
       ),
       title: Text(
