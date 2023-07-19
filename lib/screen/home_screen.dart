be49bd3c273a15276c04b361de95bf6ba76f5b1a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-19 07:55:44 +0530

    Add TabBar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -7,7 +7,16 @@ class HomePageView extends StatefulWidget {
   State<HomePageView> createState() => _HomePageViewState();
 }
 
-class _HomePageViewState extends State<HomePageView> {
+class _HomePageViewState extends State<HomePageView>
+    with SingleTickerProviderStateMixin {
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +26,23 @@ class _HomePageViewState extends State<HomePageView> {
           IconButton(icon: const Icon(Icons.search), onPressed: () {}),
           IconButton(icon: const Icon(Icons.more_vert), onPressed: () {}),
         ],
+        bottom: TabBar(
+          controller: _controller,
+          tabs: const [
+            Tab(
+              icon: Icon(Icons.camera_alt),
+            ),
+            Tab(
+              text: "CHATS",
+            ),
+            Tab(
+              text: "STATUS",
+            ),
+            Tab(
+              text: "CALLS",
+            ),
+          ],
+        ),
       ),
     );
   }
