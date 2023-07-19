5b623dabd4fe407e472e4312f1ef65a5f25f4106
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-19 11:57:31 +0530

    Add Divide

--- lib/pages/calls_page.dart
+++ lib/pages/calls_page.dart
@@ -17,8 +17,38 @@ class _CallsPageState extends State<CallsPage> {
         child: const Icon(Icons.add_ic_call_rounded),
       ),
       body: ListView(
-        children: const [
-          CustomCallCard(),
+        children: [
+          const SizedBox(
+            height: 6,
+          ),
+          ListTile(
+            leading: FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 50, 185, 156),
+              onPressed: () {},
+              child: Transform.rotate(
+                angle: 135 * 3.1415927 / 180,
+                child: const Icon(Icons.link_rounded),
+              ),
+            ),
+            title: const Text(
+              "Create call link",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: const Text(
+              "Share a link for your WhatsApp call",
+              style: TextStyle(
+                fontSize: 13,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+          const Divider(),
+          const CustomCallCard(),
         ],
       ),
     );
