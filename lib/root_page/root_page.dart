8568dda5b4678ba9291392e98c63dbab30e7aacd
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-19 14:31:54 +0530

    fixed

--- lib/root_page/root_page.dart
+++ lib/root_page/root_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/screen/home_screen.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ChatApp extends StatelessWidget {
+  const ChatApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
