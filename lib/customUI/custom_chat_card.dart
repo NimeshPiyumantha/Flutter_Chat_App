6dcd1fc75705f9b80c774c9eee138be15ec02cbb
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-04 00:25:48 +0530

    Add Divider

--- lib/customUI/custom_chat_card.dart
+++ lib/customUI/custom_chat_card.dart
@@ -6,39 +6,49 @@ class CustomChatCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.blueGrey,
-        child: SvgPicture.asset(
-          "assets/images/groups_black_36dp.svg",
-          height: 38,
-          width: 38,
-          color: Colors.white,
-        ),
-      ),
-      title: const Text(
-        "Dev Stack",
-        style: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-      subtitle: const Row(
-        children: [
-          Icon(Icons.done_all),
-          SizedBox(
-            width: 3,
-          ),
-          Text(
-            "Hi dev Stack",
-            style: TextStyle(
-              fontSize: 13,
+    return Column(
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.blueGrey,
+            child: SvgPicture.asset(
+              "assets/images/groups_black_36dp.svg",
+              height: 37,
+              width: 37,
+              color: Colors.white,
             ),
           ),
-        ],
-      ),
-      trailing: const Text('18:04'),
+          title: const Text(
+            "Dev Stack",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: const Row(
+            children: [
+              Icon(Icons.done_all),
+              SizedBox(
+                width: 3,
+              ),
+              Text(
+                "Hi dev Stack",
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              ),
+            ],
+          ),
+          trailing: const Text('18:04'),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(right: 20, left: 80),
+          child: Divider(
+            thickness: 1,
+          ),
+        ),
+      ],
     );
   }
 }
