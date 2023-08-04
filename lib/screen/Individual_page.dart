537ce3269019be28eb25d01e9ab1b62ae0bd6ec6
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-04 10:27:09 +0530

    Add IndividualPage to nav bar

--- lib/screen/Individual_page.dart
+++ lib/screen/Individual_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/model/chat_model.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class IndividualPage extends StatefulWidget {
   const IndividualPage({super.key, required this.chatModel});
@@ -12,24 +13,100 @@ class IndividualPage extends StatefulWidget {
 class _IndividualPageState extends State<IndividualPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         leadingWidth: 70,
+        titleSpacing: 0,
         leading: InkWell(
           onTap: () {
             Navigator.pop(context);
           },
-          child: const Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.arrow_back,size: 24,),
+              const Icon(
+                Icons.arrow_back,
+                size: 24,
+              ),
               CircleAvatar(
                 radius: 20,
                 backgroundColor: Colors.blueGrey,
+                child: SvgPicture.asset(
+                  widget.chatModel.isGroup
+                      ? "assets/images/groups.svg"
+                      : "assets/images/person.svg",
+                  height: 35,
+                  width: 35,
+                  color: Colors.white,
+                ),
               )
             ],
           ),
         ),
+        title: Container(
+          margin: const EdgeInsets.all(5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                widget.chatModel.name,
+                style: const TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Text(
+                "Last seen today at 12:05",
+                style: TextStyle(fontSize: 13),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.videocam),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.call),
+            onPressed: () {},
+          ),
+          PopupMenuButton<String>(
+            padding: const EdgeInsets.all(0),
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  value: "View Contact",
+                  child: Text("View Contact"),
+                ),
+                const PopupMenuItem(
+                  value: "Media, links, and docs",
+                  child: Text("Media, links, and docs"),
+                ),
+                const PopupMenuItem(
+                  value: "Whatsapp Web",
+                  child: Text("Whatsapp Web"),
+                ),
+                const PopupMenuItem(
+                  value: "Search",
+                  child: Text("Search"),
+                ),
+                const PopupMenuItem(
+                  value: "Mute Notification",
+                  child: Text("Mute Notification"),
+                ),
+                const PopupMenuItem(
+                  value: "Wallpaper",
+                  child: Text("Wallpaper"),
+                ),
+              ];
+            },
+          ),
+        ],
       ),
     );
   }
