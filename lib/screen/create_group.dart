080c4a12eb8f4fd788ab37332dd03f6fceced89d
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-17 20:42:36 +0530

    Refactored code

--- lib/screen/create_group.dart
+++ lib/screen/create_group.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../customUI/avatar_card.dart';
-import '../customUI/contact_card.dart';
+import 'package:flutter_chat_app/customUI/contact_card.dart';
 import '../model/chat_model.dart';
 
 class CreateGroup extends StatefulWidget {
@@ -29,102 +27,43 @@ class _CreateGroupState extends State<CreateGroup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "New Group",
-                style: TextStyle(
-                  fontSize: 19,
-                  fontWeight: FontWeight.bold,
-                ),
+      appBar: AppBar(
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "New Group",
+              style: TextStyle(
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                "Add participants",
-                style: TextStyle(
-                  fontSize: 13,
-                ),
-              )
-            ],
-          ),
-          actions: [
-            IconButton(
-                icon: const Icon(
-                  Icons.search,
-                  size: 26,
-                ),
-                onPressed: () {}),
+            ),
+            Text(
+              "Add participants",
+              style: TextStyle(
+                fontSize: 13,
+              ),
+            )
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: const Color(0xFF128C7E),
-            onPressed: () {},
-            child: const Icon(Icons.arrow_forward)),
-        body: Stack(
-          children: [
-            ListView.builder(
-                itemCount: contacts.length + 1,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return Container(
-                      height: groupmember.isNotEmpty ? 90 : 10,
-                    );
-                  }
-                  return InkWell(
-                    onTap: () {
-                      setState(() {
-                        if (contacts[index - 1].select == true) {
-                          groupmember.remove(contacts[index - 1]);
-                          contacts[index - 1].select = false;
-                        } else {
-                          groupmember.add(contacts[index - 1]);
-                          contacts[index - 1].select = true;
-                        }
-                      });
-                    },
-                    child: ContactCard(
-                      contact: contacts[index - 1],
-                    ),
-                  );
-                }),
-            groupmember.isNotEmpty
-                ? Align(
-                    alignment: Alignment.topCenter,
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 75,
-                          color: Colors.white,
-                          child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: contacts.length,
-                              itemBuilder: (context, index) {
-                                if (contacts[index].select == true) {
-                                  return InkWell(
-                                    onTap: () {
-                                      setState(() {
-                                        groupmember.remove(contacts[index]);
-                                        contacts[index].select = false;
-                                      });
-                                    },
-                                    child: AvatarCard(
-                                      chatModel: contacts[index],
-                                    ),
-                                  );
-                                }
-                                return Container();
-                              }),
-                        ),
-                        const Divider(
-                          thickness: 1,
-                        ),
-                      ],
-                    ),
-                  )
-                : Container(),
-          ],
-        ));
+        actions: [
+          IconButton(
+              icon: const Icon(
+                Icons.search,
+                size: 26,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return ContactCard(
+            contact: contacts[index],
+          );
+        },
+      ),
+    );
   }
 }
