3a7804bf6c5df16de3dafa5d6cc36f3626106f03
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-17 18:59:27 +0530

    Add select contact header

--- lib/screen/select_content.dart
+++ lib/screen/select_content.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class SelectContact extends StatefulWidget {
@@ -13,7 +11,29 @@ class _SelectContactState extends State<SelectContact> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Select Contact',
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                )),
+            Text(
+              "256 contacts",
+              style: TextStyle(fontSize: 13),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search, size: 26),
+          ),
+        ],
+      ),
     );
   }
 }
